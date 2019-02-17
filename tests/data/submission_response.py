938484109be6652f910d47b1a7e3d9275a92ce92
Kartik Verma <vkartik97@gmail.com>
2019-02-17 20:20:34 +0530

    Added aws_credential_response
    #145

--- tests/data/submission_response.py
+++ tests/data/submission_response.py
@@ -122,3 +122,34 @@ submission_result = """
                     "submitted_at": "2018-06-08T09:24:09.866590Z",
                     "when_made_public": null
                 }"""
+
+
+aws_credentials = """
+{
+    "federated_user": {
+        "Credentials": {
+            "AccessKeyId": "ABCDEFGHIJKLMNOPQRTUVWXYZ",
+            "SecretAccessKey": "NMgBB75gfVBCDEFGHIJK8g00qVyyzQW+4XjJGQALMNOPQRSTUV",
+            "SessionToken": "FQoGZX.....",
+            "Expiration": "7845124587452165"
+        },
+        "FederatedUser": {
+            "FederatedUserId": "1234567890:test-user",
+            "Arn": "arn:aws:sts::1234567890:federated-user/test-user"
+        },
+        "PackedPolicySize": 28,
+        "ResponseMetadata": {
+            "RequestId": "fb47f78b-2a92-11e9-84b9-33527429b818",
+            "HTTPStatusCode": 200,
+            "HTTPHeaders": {
+                "x-amzn-requestid": "fb47f78b-2a92-11e9-84b9-33527429b818",
+                "content-type": "text/xml",
+                "content-length": "1245",
+                "date": "Thu, 07 Feb 2019 04:43:57 GMT"
+            },
+            "RetryAttempts": 0
+        }
+    },
+    "docker_repository_uri": "ABCDEFGHIJKLMNOPQRTUVWXYZ"
+}
+"""
